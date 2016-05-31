9561c0cd3a240bb01c9b1409af505d323d21a13a
MariaWatters <mariakwatters@gmail.com>
2016-05-30 18:36:17 -0700

    The if loop has been fixed so that only one reading of the sensor gets compared to the two color options

--- IR-Sensor-Calibration/IR-Sensor-Calibration.ino
+++ IR-Sensor-Calibration/IR-Sensor-Calibration.ino
@@ -29,8 +29,18 @@ void setup() {
 // This is the code that continuously loops until the Arduino is turns off.
 void loop(){
   /* First you need to send a faux sinewave to the LED to turn it on and off.
-   * Do this by following code:
+   * Since a wave with a frequency of 38.5 kHz has a period of about 26 
+   * microseconds, turning it on for half that time would be half a period,
+   * which is where int halfPeriod comes from. The cycles comes from the fact
+   * that these sensors detect the light wave's reflection and you need to pulse
+   * the light multiple times to create a continuous 'wave' that lasts about 
+   * 1 millisecond. Since one crest to crest pulse takes 26 microseconds (13 for 
+   * a half period), and there are 1000 microseconds in a millisecond, you would 
+   * have to turn the LED on and then off (which makes one complete period) 
+   * 1000/26 times, which is rounded to 38 times. Here I used a for loop to ensure 
+   * that a the command to turn the LED on and off was looped exactly 38 times.
    */
+// Try turning this into a seperate function.
   int halfPeriod = 13;
   int cycles = 38;
   int i;
@@ -40,19 +50,22 @@ void loop(){
     digitalWrite(A3, LOW);
     delayMicroseconds(halfPeriod - 1); 
   }
-  
-  if(digitalRead(A0)==HIGH){
+
+/* HIGH/LOW are boolean values so I can use them as case comparisons.
+ * I also defined the variable color as a bool type outside of the if loop
+ * so that I maintained the nature of the HIGH/LOW returned by digitalRead()
+ * and made it so a single reading of the sensor gets compared to two options.
+ */
+  bool color = digitalRead(A0);
+  if(color == true){
    Serial.println("White");
    // This will delay the program's loops by 1.5 seconds 
-   // Arduino uses a base of milliseconds
+   // (Arduino uses a base of milliseconds)
    delay(150);
-   } else if (digitalRead(A0)==LOW){
+   } else if (color == false){
    Serial.println("Black");
    delay(150);
    } else {
     Serial.println("Nothing detected");
    }
 }
-/* As is, the sensors are only returning "White" and nothing else.
- * Which means it is only detecting HIGH
- */
